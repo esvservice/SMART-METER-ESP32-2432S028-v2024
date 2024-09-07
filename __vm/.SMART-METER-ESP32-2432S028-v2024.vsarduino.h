5c0c49a4158a0183647a44d40981cdb156baf8d9
ON7LSP <on7lsp@gmail.com>
2024-09-07 10:50:54 +0200

    a

---
+++ __vm/.SMART-METER-ESP32-2432S028-v2024.vsarduino.h
@@ -0,0 +1,28 @@
+/* 
+	Editor: https://www.visualmicro.com/
+			This file is for intellisense purpose only.
+			Visual micro (and the arduino ide) ignore this code during compilation. This code is automatically maintained by visualmicro, manual changes to this file will be overwritten
+			The contents of the _vm sub folder can be deleted prior to publishing a project
+			All non-arduino files created by visual micro and all visual studio project or solution files can be freely deleted and are not required to compile a sketch (do not delete your own code!).
+			Note: debugger breakpoints are stored in '.sln' or '.asln' files, knowledge of last uploaded breakpoints is stored in the upload.vmps.xml file. Both files are required to continue a previous debug session without needing to compile and upload again
+	
+	Hardware: ESP32 Dev Module(esp32_esp32), Platform=esp32, Package=esp32
+*/
+
+#if defined(_VMICRO_INTELLISENSE)
+
+#ifndef _VSARDUINO_H_
+#define _VSARDUINO_H_
+#include <arduino.h>
+#include <pins_arduino.h> 
+#include "..\SMART-METER-ESP32-2432S028 v2024.ino"
+#include "..\WifiManager32.ino"
+#include "..\cat.ino"
+#include "..\display.ino"
+#include "..\hardswtich.ino"
+#include "..\serialcontrol.ino"
+#include "..\setup.ino"
+#include "..\touchpad.ino"
+#include "..\touchswitch.ino"
+#endif
+#endif
